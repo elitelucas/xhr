60fabc52372ef9a24c6312ca8889b6879b5ffa9e
vladimir9255 <69668555+vladimir9255@users.noreply.github.com>
2021-04-27 13:39:39 +0900

    Update funs.php

--- chat/Applications/Chat/funs.php
+++ chat/Applications/Chat/funs.php
@@ -33,6 +33,9 @@ function decode($str){
  * @desc 连接CacheRedis缓存数据库
  */
 function initCacheRedis() {
+    echo RD_HOST;
+    echo RD_PORT;
+    die;
     //该参数待放入配置文件
     $cache_redis = new redis();
     $ret = $cache_redis->connect(RD_HOST, RD_PORT);
